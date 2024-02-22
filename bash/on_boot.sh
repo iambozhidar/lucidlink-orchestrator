20a59329ae09d74015e11f0e82f62d091e14a68d
bozhidar.stoyanov <bozhidar.stoyanov@epicdevs.io>
2024-02-22 09:01:48 +0200

    Update the EC2's on_boot.sh file.

--- bash/on_boot.sh
+++ bash/on_boot.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-# Run the ec2-metadata command to get the instance ID
-# The output will be in the format "instance-id: i-1234567890abcdef0"
+# The value will be in the format "instance-id: i-1234567890abcdef0"
 # So, we use cut to extract the second part (the actual instance ID)
-INSTANCE_ID=$(ec2-metadata -i | cut -d " " -f 2)
-
-# Print the instance ID
-echo "Instance ID: $INSTANCE_ID"
+instance_id=$(ec2-metadata -i | cut -d " " -f 2)
+aws ssm put-parameter --name $instance_id --value "320,1000,3000" --type "String" --overwrite
