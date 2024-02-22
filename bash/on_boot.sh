2578018f1df3db788bc88359f56f964f5039173b
bozhidar.stoyanov <bozhidar.stoyanov@epicdevs.io>
2024-02-22 10:32:21 +0200

    Update on_boot.sh to convert time measurements to milliseconds and put them in SSM parameter.

--- bash/on_boot.sh
+++ bash/on_boot.sh
@@ -1,20 +1,16 @@
 #!/bin/bash -e
 
-## The value will be in the format "instance-id: i-1234567890abcdef0"
-## So, we use cut to extract the second part (the actual instance ID)
-#instance_id=$(ec2-metadata -i | cut -d " " -f 2)
-#aws ssm put-parameter --name $instance_id --value "320,1000,3000" --type "String" --overwrite
-
-
 # Function to convert time format to total seconds
-convert_to_seconds() {
+convert_time_str_to_ms() {
     # Split input based on 'm' and 's' and remove 's' suffix
     local time_str=$1
     local minutes=$(echo $time_str | cut -d'm' -f1)
     local seconds=$(echo $time_str | cut -d'm' -f2 | sed 's/s//')
 
-    # Convert to total seconds
-    echo "$minutes * 60 + $seconds" | bc
+    # Convert to total milliseconds (1 minute = 60000 milliseconds, 1 second = 1000 milliseconds)
+    # Then format the output as an integer to remove trailing zeros
+    local total_milliseconds=$(echo "($minutes * 60 + $seconds) * 1000" | bc)
+    printf "%.0f" "$total_milliseconds" # This will format the floating point number to an integer
 }
 
 # Create source and destination directories
@@ -26,7 +22,7 @@ create_output=$( { time (for ((i=1; i<=1000; i++)); do touch "source/file$i.txt"
 create_real=$(echo "$create_output" | grep real | awk '{print $2}')
 
 # Convert create_real to total seconds
-create_seconds=$(convert_to_seconds $create_real)
+creation_time_ms=$(convert_time_str_to_ms $create_real)
 
 # Copy files from source to destination
 echo "Copying files from source to destination..."
@@ -34,7 +30,7 @@ copy_output=$( { time (cp -r source/* destination/;) ; } 2>&1 )
 copy_real=$(echo "$copy_output" | grep real | awk '{print $2}')
 
 # Convert copy_real to total seconds
-copy_seconds=$(convert_to_seconds $copy_real)
+copy_time_ms=$(convert_time_str_to_ms $copy_real)
 
 # Delete files from source
 echo "Deleting files from source..."
@@ -42,9 +38,14 @@ delete_output=$( { time (rm -rf source/*;) ; } 2>&1 )
 delete_real=$(echo "$delete_output" | grep real | awk '{print $2}')
 
 # Convert delete_real to total seconds
-delete_seconds=$(convert_to_seconds $delete_real)
+deletion_time_ms=$(convert_time_str_to_ms $delete_real)
 
 # Print real time for each operation in seconds
-echo "Time to create files (seconds): $create_seconds"
-echo "Time to copy files (seconds): $copy_seconds"
-echo "Time to delete files (seconds): $delete_seconds"
+echo "Time to create files (ms): $creation_time_ms"
+echo "Time to copy files (ms): $copy_time_ms"
+echo "Time to delete files (ms): $deletion_time_ms"
+
+# The value will be in the format "instance-id: i-1234567890abcdef0"
+# So, we use cut to extract the second part (the actual instance ID)
+instance_id=$(ec2-metadata -i | cut -d " " -f 2)
+aws ssm put-parameter --name $instance_id --value "$creation_time_ms,$copy_time_ms,$deletion_time_ms" --type "String" --overwrite
