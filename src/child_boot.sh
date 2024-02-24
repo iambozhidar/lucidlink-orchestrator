320c3567eca1ab5c495492d5299c14dc55832755
bozhidar.stoyanov <bozhidar.stoyanov@epicdevs.io>
2024-02-24 11:45:49 +0200

    Make the child boot script log stdout and stderr in a log file to enable inspection.

--- src/child_boot.sh
+++ src/child_boot.sh
@@ -12,24 +12,28 @@ time_ms() {
     echo "$time_sec * 1000" | bc | xargs printf "%.0f\n"
 }
 
-# Create source and destination directories
-mkdir -p source destination
+main() {
+    # Create source and destination directories
+    mkdir -p source destination
 
-echo "Creating 1,000 files in source directory..."
-creation_time_ms=$(time_ms "for ((i=1; i<=1000; i++)); do touch \"source/file\$i.txt\"; done")
+    echo "Creating 1,000 files in source directory..."
+    creation_time_ms=$(time_ms "for ((i=1; i<=1000; i++)); do touch \"source/file\$i.txt\"; done")
 
-echo "Copying files from source to destination..."
-copy_time_ms=$(time_ms "cp -r source/* destination/")
+    echo "Copying files from source to destination..."
+    copy_time_ms=$(time_ms "cp -r source/* destination/")
 
-echo "Deleting files from source..."
-deletion_time_ms=$(time_ms "{ rm -rf source/*; rm -rf destination/*; }")
+    echo "Deleting files from source..."
+    deletion_time_ms=$(time_ms "{ rm -rf source/*; rm -rf destination/*; }")
 
-# Print time in milliseconds for each operation
-echo "Time to create files (ms): $creation_time_ms"
-echo "Time to copy files (ms): $copy_time_ms"
-echo "Time to delete files (ms): $deletion_time_ms"
+    # Print time in milliseconds for each operation
+    echo "Time to create files (ms): $creation_time_ms"
+    echo "Time to copy files (ms): $copy_time_ms"
+    echo "Time to delete files (ms): $deletion_time_ms"
 
-# Get instance id from metadata and write time results to SSM with the id as the parameter's name
-# The id value will be in the format "instance-id: i-1234567890abcdef0" so we 'cut' to the second part
-instance_id=$(ec2-metadata -i | cut -d " " -f 2)
-aws ssm put-parameter --name "$instance_id" --value "$creation_time_ms,$copy_time_ms,$deletion_time_ms" --type "String" --overwrite
+    # Get instance id from metadata and write time results to SSM with the id as the parameter's name
+    # The id value will be in the format "instance-id: i-1234567890abcdef0" so we 'cut' to the second part
+    instance_id=$(ec2-metadata -i | cut -d " " -f 2)
+    aws ssm put-parameter --name "$instance_id" --value "$creation_time_ms,$copy_time_ms,$deletion_time_ms" --type "String" --overwrite
+}
+
+main "$@" > /var/log/child_boot.log 2>&1
